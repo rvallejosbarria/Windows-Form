a1b8171bff0923730a1932f5cfbce49f41f1ffaa
Roberto Vallejos <rvallejosbarria@gmail.com>
2022-09-17 19:46:39 -0400

    Finished GUI

--- HelloWorld/Form1.Designer.vb
+++ HelloWorld/Form1.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.btnSave = New System.Windows.Forms.Button()
         Me.lblMessage = New System.Windows.Forms.Label()
         Me.lblCompany = New System.Windows.Forms.Label()
@@ -30,6 +31,17 @@ Partial Class Form1
         Me.cbService = New System.Windows.Forms.ComboBox()
         Me.lblActivity = New System.Windows.Forms.Label()
         Me.cbActivity = New System.Windows.Forms.ComboBox()
+        Me.lblForStartDate = New System.Windows.Forms.Label()
+        Me.lblStartDate = New System.Windows.Forms.Label()
+        Me.lblStartTime = New System.Windows.Forms.Label()
+        Me.lblForStartTime = New System.Windows.Forms.Label()
+        Me.lblForDuration = New System.Windows.Forms.Label()
+        Me.lblDuration = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.cbUser = New System.Windows.Forms.ComboBox()
+        Me.lblUser = New System.Windows.Forms.Label()
+        Me.txtMessage = New System.Windows.Forms.TextBox()
+        Me.lblBody = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'btnSave
@@ -52,7 +64,7 @@ Partial Class Form1
         'lblCompany
         '
         Me.lblCompany.AutoSize = True
-        Me.lblCompany.Location = New System.Drawing.Point(12, 9)
+        Me.lblCompany.Location = New System.Drawing.Point(12, 71)
         Me.lblCompany.Name = "lblCompany"
         Me.lblCompany.Size = New System.Drawing.Size(76, 20)
         Me.lblCompany.TabIndex = 2
@@ -61,7 +73,7 @@ Partial Class Form1
         'cbCompany
         '
         Me.cbCompany.FormattingEnabled = True
-        Me.cbCompany.Location = New System.Drawing.Point(94, 6)
+        Me.cbCompany.Location = New System.Drawing.Point(94, 68)
         Me.cbCompany.Name = "cbCompany"
         Me.cbCompany.Size = New System.Drawing.Size(121, 28)
         Me.cbCompany.TabIndex = 3
@@ -69,7 +81,7 @@ Partial Class Form1
         'lblService
         '
         Me.lblService.AutoSize = True
-        Me.lblService.Location = New System.Drawing.Point(27, 43)
+        Me.lblService.Location = New System.Drawing.Point(27, 139)
         Me.lblService.Name = "lblService"
         Me.lblService.Size = New System.Drawing.Size(61, 20)
         Me.lblService.TabIndex = 4
@@ -78,7 +90,7 @@ Partial Class Form1
         'cbService
         '
         Me.cbService.FormattingEnabled = True
-        Me.cbService.Location = New System.Drawing.Point(94, 40)
+        Me.cbService.Location = New System.Drawing.Point(94, 136)
         Me.cbService.Name = "cbService"
         Me.cbService.Size = New System.Drawing.Size(121, 28)
         Me.cbService.TabIndex = 5
@@ -86,7 +98,7 @@ Partial Class Form1
         'lblActivity
         '
         Me.lblActivity.AutoSize = True
-        Me.lblActivity.Location = New System.Drawing.Point(30, 77)
+        Me.lblActivity.Location = New System.Drawing.Point(30, 173)
         Me.lblActivity.Name = "lblActivity"
         Me.lblActivity.Size = New System.Drawing.Size(58, 20)
         Me.lblActivity.TabIndex = 6
@@ -95,16 +107,118 @@ Partial Class Form1
         'cbActivity
         '
         Me.cbActivity.FormattingEnabled = True
-        Me.cbActivity.Location = New System.Drawing.Point(94, 74)
+        Me.cbActivity.Location = New System.Drawing.Point(94, 170)
         Me.cbActivity.Name = "cbActivity"
         Me.cbActivity.Size = New System.Drawing.Size(121, 28)
         Me.cbActivity.TabIndex = 7
+        '
+        'lblForStartDate
+        '
+        Me.lblForStartDate.AutoSize = True
+        Me.lblForStartDate.Location = New System.Drawing.Point(90, 5)
+        Me.lblForStartDate.Name = "lblForStartDate"
+        Me.lblForStartDate.Size = New System.Drawing.Size(119, 20)
+        Me.lblForStartDate.TabIndex = 8
+        Me.lblForStartDate.Text = "Fecha de inicio:"
+        '
+        'lblStartDate
+        '
+        Me.lblStartDate.AutoSize = True
+        Me.lblStartDate.Location = New System.Drawing.Point(90, 25)
+        Me.lblStartDate.Name = "lblStartDate"
+        Me.lblStartDate.Size = New System.Drawing.Size(91, 20)
+        Me.lblStartDate.TabIndex = 9
+        Me.lblStartDate.Text = "mm-dd-yyyy"
+        '
+        'lblStartTime
+        '
+        Me.lblStartTime.AutoSize = True
+        Me.lblStartTime.Location = New System.Drawing.Point(278, 25)
+        Me.lblStartTime.Name = "lblStartTime"
+        Me.lblStartTime.Size = New System.Drawing.Size(77, 20)
+        Me.lblStartTime.TabIndex = 11
+        Me.lblStartTime.Text = "hh:mm:ss"
+        '
+        'lblForStartTime
+        '
+        Me.lblForStartTime.AutoSize = True
+        Me.lblForStartTime.Location = New System.Drawing.Point(278, 5)
+        Me.lblForStartTime.Name = "lblForStartTime"
+        Me.lblForStartTime.Size = New System.Drawing.Size(109, 20)
+        Me.lblForStartTime.TabIndex = 10
+        Me.lblForStartTime.Text = "Hora de inicio:"
+        '
+        'lblForDuration
+        '
+        Me.lblForDuration.AutoSize = True
+        Me.lblForDuration.Location = New System.Drawing.Point(278, 116)
+        Me.lblForDuration.Name = "lblForDuration"
+        Me.lblForDuration.Size = New System.Drawing.Size(77, 20)
+        Me.lblForDuration.TabIndex = 12
+        Me.lblForDuration.Text = "Duración:"
+        '
+        'lblDuration
+        '
+        Me.lblDuration.AutoSize = True
+        Me.lblDuration.Location = New System.Drawing.Point(278, 136)
+        Me.lblDuration.Name = "lblDuration"
+        Me.lblDuration.Size = New System.Drawing.Size(71, 20)
+        Me.lblDuration.TabIndex = 13
+        Me.lblDuration.Text = "00:00:00"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'cbUser
+        '
+        Me.cbUser.FormattingEnabled = True
+        Me.cbUser.Location = New System.Drawing.Point(94, 102)
+        Me.cbUser.Name = "cbUser"
+        Me.cbUser.Size = New System.Drawing.Size(121, 28)
+        Me.cbUser.TabIndex = 15
+        '
+        'lblUser
+        '
+        Me.lblUser.AutoSize = True
+        Me.lblUser.Location = New System.Drawing.Point(45, 105)
+        Me.lblUser.Name = "lblUser"
+        Me.lblUser.Size = New System.Drawing.Size(43, 20)
+        Me.lblUser.TabIndex = 14
+        Me.lblUser.Text = "User"
+        '
+        'txtMessage
+        '
+        Me.txtMessage.Location = New System.Drawing.Point(16, 230)
+        Me.txtMessage.Multiline = True
+        Me.txtMessage.Name = "txtMessage"
+        Me.txtMessage.Size = New System.Drawing.Size(200, 104)
+        Me.txtMessage.TabIndex = 16
+        '
+        'lblBody
+        '
+        Me.lblBody.AutoSize = True
+        Me.lblBody.Location = New System.Drawing.Point(14, 207)
+        Me.lblBody.Name = "lblBody"
+        Me.lblBody.Size = New System.Drawing.Size(74, 20)
+        Me.lblBody.TabIndex = 17
+        Me.lblBody.Text = "Message"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblBody)
+        Me.Controls.Add(Me.txtMessage)
+        Me.Controls.Add(Me.cbUser)
+        Me.Controls.Add(Me.lblUser)
+        Me.Controls.Add(Me.lblDuration)
+        Me.Controls.Add(Me.lblForDuration)
+        Me.Controls.Add(Me.lblStartTime)
+        Me.Controls.Add(Me.lblForStartTime)
+        Me.Controls.Add(Me.lblStartDate)
+        Me.Controls.Add(Me.lblForStartDate)
         Me.Controls.Add(Me.cbActivity)
         Me.Controls.Add(Me.lblActivity)
         Me.Controls.Add(Me.cbService)
@@ -128,4 +242,15 @@ Partial Class Form1
     Friend WithEvents cbService As ComboBox
     Friend WithEvents lblActivity As Label
     Friend WithEvents cbActivity As ComboBox
+    Friend WithEvents lblForStartDate As Label
+    Friend WithEvents lblStartDate As Label
+    Friend WithEvents lblStartTime As Label
+    Friend WithEvents lblForStartTime As Label
+    Friend WithEvents lblForDuration As Label
+    Friend WithEvents lblDuration As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents cbUser As ComboBox
+    Friend WithEvents lblUser As Label
+    Friend WithEvents txtMessage As TextBox
+    Friend WithEvents lblBody As Label
 End Class
--- HelloWorld/Form1.vb
+++ HelloWorld/Form1.vb
@@ -1,42 +1,26 @@
 ﻿Imports MySql.Data.MySqlClient
 
 Public Class Form1
+    Private timer As Integer
+
+    Private hours As Integer = 0
+    Private minutes As Integer = 0
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim companies = New String() {"PANG", "TGN"}
+        Dim users = New String() {"Usuario1", "Usuario2", "Usuario3"}
         Dim services = New String() {"Soporte", "Plataforma"}
 
         cbCompany.Items.AddRange(companies)
+        cbUser.Items.AddRange(users)
         cbService.Items.AddRange(services)
+
+        lblStartDate.Text = DateString
+        lblStartTime.Text = TimeString
+
+        Timer1.Start()
     End Sub
-    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
-        Dim MySqlConn As New MySqlConnection
 
-        MySqlConn.ConnectionString = ("")
-
-        Try
-            MySqlConn.Open()
-
-            MessageBox.Show("Connection to Database has been opened.")
-        Catch myerror As MySqlException
-            MessageBox.Show("Cannot connect to database: " & myerror.Message)
-        Finally
-
-            MySqlConn.Dispose()
-
-        End Try
-
-        Try
-            MySqlConn.Close()
-
-            MessageBox.Show("Connection to Database has been Closed.")
-        Catch myerror As MySqlException
-            MessageBox.Show("Cannot connect to database: " & myerror.Message)
-        Finally
-            MySqlConn.Dispose()
-        End Try
-
-        lblMessage.Text = "Saved!"
-    End Sub
     Private Sub cbService_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbService.SelectedIndexChanged
         cbActivity.Items.Clear()
 
@@ -54,5 +38,60 @@ Public Class Form1
         End Select
 
         cbActivity.Items.AddRange(activities)
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        If cbCompany.Text.Equals("") Or
+            cbUser.Text.Equals("") Or
+            cbService.Text.Equals("") Or
+            cbActivity.Text.Equals("") Then
+            lblMessage.Text = "All fields must be completed!"
+        Else
+            Dim MySqlConn As New MySqlConnection
+
+            MySqlConn.ConnectionString = ("")
+
+            lblMessage.Text = ""
+
+            Try
+                MySqlConn.Open()
+
+                MessageBox.Show("Connection to Database has been opened.")
+            Catch myerror As MySqlException
+                MessageBox.Show("Cannot connect to database: " & myerror.Message)
+            Finally
+                MySqlConn.Dispose()
+            End Try
+
+            Try
+                MySqlConn.Close()
+
+                MessageBox.Show("Connection to Database has been Closed.")
+            Catch myerror As MySqlException
+                MessageBox.Show("Cannot connect to database: " & myerror.Message)
+            Finally
+                MySqlConn.Dispose()
+            End Try
+
+            lblMessage.Text = "Saved!"
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        timer += 1
+
+        If timer < 60 Then
+            lblDuration.Text = If((hours < 10), "0" & hours.ToString, hours)
+            lblDuration.Text += ":" & If((minutes < 10), "0" & minutes.ToString, minutes)
+            lblDuration.Text += ":" & If((timer < 10), "0" & timer.ToString, timer)
+        ElseIf timer = 60 Then
+            minutes += 1
+            timer = 0
+
+            If minutes = 60 Then
+                hours += 1
+                minutes = 0
+            End If
+        End If
     End Sub
 End Class
